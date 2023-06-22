f59eee992a05be491e44549e366bb7ce3e51299d
FrancoCambi <106709849+FrancoCambi@users.noreply.github.com>
2023-06-22 04:37:00 -0300

    Delete cpp directory

--- cpp/Estructuras/GList/main.hpp
+++
@@ -1,6 +0,0 @@
-#ifndef __MAIN_HPP__
-#define __MAIN_HPP__
-
-#include"glist.hpp"
-
-#endif /* __MAIN_H__ */
--- cpp/test/BSTree/main.hpp
+++
@@ -1,7 +0,0 @@
-#ifndef __MAIN_HPP__
-#define __MAIN_HPP__
-
-#include"bstree.hpp"
-#include"../GList/glist.hpp"
-
-#endif /* __MAIN_HPP__ */
